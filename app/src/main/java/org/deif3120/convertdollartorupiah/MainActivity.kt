d0252f192212cf7252a75c0629a862c67e8f0ee6
ahmadroji <ahmadroji@student.telkomuniversity.ac.id>
2023-03-30 20:24:14 +0700

    tema dan convert

--- app/src/main/java/org/deif3120/convertdollartorupiah/MainActivity.kt
+++ app/src/main/java/org/deif3120/convertdollartorupiah/MainActivity.kt
@@ -2,10 +2,32 @@ package org.deif3120.convertdollartorupiah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.deif3120.convertdollartorupiah.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConvert.setOnClickListener {
+            val dollar = binding.dollarInp.text.toString().trim()
+            var rp = 0
+
+            when{
+                TextUtils.isEmpty(dollar) -> {
+                    Toast.makeText(this,"masukan jumlah dollar!", Toast.LENGTH_SHORT).show()
+                    binding.dollarInp.requestFocus()
+                }
+                else -> {
+                    rp = dollar.toInt() * 15041
+                    binding.tvRupiah.text =rp.toString()
+
+                }
+            }
+        }
     }
 }
